291142207880b8fa1650cfb272d6ad0999c9e45e
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2024-05-02 16:56:19 +0200

    Update hypervolume.R

--- hypervolume.R
+++ hypervolume.R
@@ -6,7 +6,7 @@ library(ClimDatDownloadR)
 # Alternatively, a direct link with GitHub can be created
 if(!require(devtools)) install.packages("devtools")
 library(devtools)
-devtools::install_github("HelgeJentsch/ClimDatDownloadR")
+# devtools::install_github("HelgeJentsch/ClimDatDownloadR")
 library(raster)
 library(viridis)
 library(corrplot)
@@ -21,38 +21,36 @@ library(gdata)
 install.packages("hypervolume")
 devtools::install_github("bblonder/hypervolume")
 
-# git add 
-
-## Upload input data
-# Set working directory
-
 getwd()
-setwd("/media/r_projects/phd_rocio/hackathon_project_8/shapefiles/")
+setwd("/media/r_projects/phd_rocio/hackathon_project_8/shapefiles")
 
 # Upload shapefile
 aoi <- st_read("parma.shp")
 aoi <- aoi$geometry
 plot(aoi)
 
+
 setwd("/media/r_projects/phd_rocio/hackathon_project_8")
 
-getwd()
 # String containing the names of raster files
-rastlist <- list.files(path ="bioclim_rasters", pattern = "CHELSA", full.names = TRUE)
+rastlist <- list.files(path ="/media/r_projects/phd_rocio/hackathon_project_8/raster_parma", pattern = "CHELSA", full.names = TRUE)
 
 # Using the list of names, all the files are imported into a single raster package
 mydata <- stack(rastlist)
 
 # Change data names
-names(mydata) <- c("mean annual T", "annual range air T", "annual precip", "amount of prec. wettest month", "amount prec. driest month")
-
-# Plot all data
-plot(mydata)
+names(mydata) <- c("mean annual T", "annual precip", "amount of prec. wettest month", "amount prec. driest month")
 
 # bio1
 plot(mydata[[1]], col = magma(500, alpha = 1, begin = 0, end = 1, direction = 1))
 
+## Correlation Matrix
+# Subsample 10% of pixels and calculate pairwise correlations
+r1 <- mydata$mean.annual.T
+cor <- cor(sampleRandom(mydata, size= ncell(r1) * 0.90 ), method = "pearson")
 
+# Plot correlation matrix
+df <- corrplot(cor, method = "number", col =  magma(30, alpha = 1, begin = 0, end = 1, direction = 1), type = "lower", tl.pos = 'ld')
 
 ## Random Virtual Species
 # Suitability map generation
@@ -67,6 +65,7 @@ random.sp <- generateRandomSp(raster.stack = mydata,
                               # Realistic species
                               realistic.sp = TRUE,
                               plot = FALSE)
+
 plot(random.sp$suitab.raster, col = magma(500, alpha = 1, begin = 0, end = 1, direction = 1))
 
 # Response functions
@@ -82,7 +81,7 @@ plot(new.pres$pa.raster)
 # Occurences
 # passi da 200, tutte le occorrenze di deh
 presence.points <- sampleOccurrences(new.pres,
-                                     n = 500,
+                                     n =1000,
                                      type = "presence only",
                                      sample.prevalence = 0.9,
                                      error.probability = 0,
@@ -93,6 +92,7 @@ presence.points <- sampleOccurrences(new.pres,
 plot(mydata[[1]], col = magma(500, alpha = 1, begin = 0, end = 1, direction = 1))
 points(presence.points$sample.points, col = "black", pch = 19)
 
+dev.off()
 
 
 ### Z transform
@@ -100,142 +100,161 @@ for (i in 1:nlayers(mydata)){
   mydata[[i]] <- (mydata[[i]] - cellStats(mydata[[i]], 'mean')) / cellStats(mydata[[i]], 'sd') 
 }
 
-plot(mydata)
+# This step is about re-calibrate the raster values: crs is still the same
+mydata
 
 ## Preliminary Steps for Niche Analysis
 # Raster with presence/absence points (step 2): used to create the realized niche
 # It must be a RasterLayer object
 raster01 <- new.pres$pa.raster %>% raster()
+raster01
 
 # The bioclimatic layers are extracted one by one from the stack
 r1 <- mydata$mean.annual.T
-r2 <- mydata$annual.range.air.T
+# r2 <- mydata$annual.range.air.T
 r3 <- mydata$annual.precip
 r4 <- mydata$amount.of.prec..wettest.month
 r5 <- mydata$amount.prec..driest.month
 
 # A stack is created containing the bioclimatic variables and the raster of presence/absence (realized niche)
-stack_pa <- brick(r1, r2, r3, r4, r5, raster01)
+stack_pa <- brick(r1, r3, r4, r5, raster01)
 
 # The values are extracted from the stack, converted into a dataframe, and only the presence pixels (1) are retained
 values <- stack_pa %>% rasterToPoints() %>% as.data.frame()
 filtered_pa <- values %>% filter(., lyr.1 == 1) %>% as.data.frame()
-filtered_pa
+filtered_pa <- filtered_pa[,-7]
+
+# Nicchia realizzata con rispettivi valori climatici (step 2)
+realized_niche_values <- filtered_pa[,-(1:2)]
+realized_niche_values
+
+# 1050 cells represent the realized niche values
+# Lets calculate the hypervolume of the realized niche first
+nrow(realized_niche_values)
+?distinct
 
 
-# Conversion from a dataframe to a raster
-raster_pa <- filtered_pa %>% .[,-8] %>% rasterFromXYZ()
-plot(raster_pa)
+############################# Function for hypervolume ############
+# Hypervolume
+calcola_ipervolume <- function(data) {
+  hv_occ <- hypervolume_gaussian(data)
+  return(hv_occ@Volume)
+ }
 
-filtered_pa <- filtered_pa[,-8]
-# The raster of occurrences is transformed into a dataset, from which the rows satisfying both conditions Real = 1 and Observed = 1 are preserved
-raster_occurences <- presence.points$sample.points %>% as.data.frame() %>% .[.$Real == 1 & .$Observed == 1, ]
-
-# The environmental variables are associated with the occurrences using their coordinates
-stack_occ <- brick(r1, r2, r3, r4, r5)
-values_occ <- stack_occ %>% rasterToPoints() %>% as.data.frame()
-filtered_occ <- merge(values_occ, raster_occurences, by = c("x", "y"))
-filtered_occ <- filtered_occ[,-(8:9)]
-filtered_occ
-
-# Aggiungi l'ID direttamente al dataset filtered_occ
-filtered_occ <- filtered_occ %>%
-  mutate(ID = row_number())
-
-
-
-# Warning messages:
-#1: In hypervolume(filtered_pa) : 
-#  Consider removing some axes.
-# 2: In hypervolume(filtered_pa) :
-#  Log number of observations (6.96) is less than or equal to the number of dimensions (7).
-# You may not have enough data to accurately estimate a hypervolume with this dimensionality.
-# Consider reducing the dimensionality of the analysis.
-
-# Ordina il dataset in base alla colonna 'ID'
-filtered_occ <- filtered_occ[order(filtered_occ$ID), ]
-filtered_occ
-
-
+# Random increment
 pippo <- function(x, no){
+  # Una riga a caso dal dataset occorrenze: inizializza
   fx <- x %>% 
     sample_n(size = 1) 
+  
   ipervolumi <- 0
   num_occurrences <- 0
   
-  for (i in 1:10000){
+  for (i in 1:1000) {
+    
+    # Al valore di inizio (una riga)
+    # Scelgo a caso no valori
+    # Li lego ad fx
+    # Mantengo valori univoci
     fx <- x %>% 
       sample_n(size = no) %>% 
       bind_rows(fx) %>% 
       distinct()
     
-    # Calcola l'ipervolume per il subset di occorrenze
+    # Ipervolume per subset
     hv <- calcola_ipervolume(fx)
     
     # Salva l'ipervolume e il numero di occorrenze
     ipervolumi <- c(ipervolumi, hv)
     num_occurrences <- c(num_occurrences, nrow(fx))
     
+    # Condizione
+    # Interrompi quando il subset ha lo stesso numero di occorrenze del set originale
     if(nrow(fx) == nrow(x)) break   
   }
   bind_cols(iperv = ipervolumi, n_occ = num_occurrences)
   
-  
+  }
+
+############################# Realized Niche Test ########### ####################
+lista_output <- list()
+valori <- c(100, 200, 300, 400, 600, 800, 1000)
+
+# Loop for the function 
+for (i in seq_along(valori_n_occ)) {
+  pluto <- pippo(realized_niche_values, valori_n_occ[i])
+  lista_output[[i]] <- pluto
 }
 
-filtered_occ %>% distinct() %>% nrow()     
+lista_output[[1]]
 
-pluto <- pippo(filtered_occ, 200)
-
-pluto
-# Inizializza un vettore per salvare gli ipervolumi
-ipervolumi <- numeric()
-num_occurrences <- numeric()
-
-# Funzione per calcolare l'ipervolume
-calcola_ipervolume <- function(data) {
-  hv_occ <- hypervolume(data)
-  return(hv_occ@Volume)
-}
-end <- as.numeric(nrow(filtered_occ))
-end
-# Ciclo for per incrementare il numero di occorrenze da 10 a 100
-for (i in seq(10, end, 10)) {
-  # Seleziona le prime 'i' occorrenze
-  subset_occ <- filtered_occ[1:i, ]
-  
-  # Calcola l'ipervolume per il subset di occorrenze
-  hv <- calcola_ipervolume(subset_occ)
-  
-  # Salva l'ipervolume e il numero di occorrenze
-  ipervolumi <- c(ipervolumi, hv)
-  num_occurrences <- c(num_occurrences, i)
-}
-
-# Crea il dataframe
-df <- data.frame(n_occurrences = num_occurrences, hypervolume = ipervolumi)
-pluto
-# Stampare il dataframe
-print(df)
-
-
-# Creazione del plot con ggplot
-plot <- ggplot(df, aes(x = n_occurrences, y = hypervolume)) +
-  geom_line() +  # Linea spezzata
+# Plot con ggplot2
+ggplot(lista_output[[3]], aes(x = n_occ, y = iperv)) +
   geom_point() + # Punti
-  labs(x = "Numero di Occorrenze", y = "Ipervolume") +  # Etichette degli assi
-  ggtitle("Ipervolume all'aumentare delle occorrenze") +   # Titolo del grafico
-  theme_minimal() +  # Stile del tema, minimalista 
-  theme_bw()
+  geom_smooth(method = "loess", se = TRUE) +  # Linea di interpolazione
+  labs(x = "n_occ", y = "iperv", title = "graph") +
+  theme_minimal()
 
-# Visualizza il plot
-print(plot)
+?geom_smooth
+
+### Tutti insieme
+dataframe_outputs <- do.call(rbind, lista_output)
+
+############################# Occurrences ###################
+# Occurrences
+# Conversion from a dataframe to a raster (to plot)
+raster_pa <- filtered_pa %>% .[,-7] %>% rasterFromXYZ()
+plot(raster_pa)
 
 
-### Roadside
+# The raster of occurrences is transformed into a dataset, from which the rows satisfying both conditions Real = 1 and Observed = 1 are preserved
+raster_occurences <- presence.points$sample.points %>% as.data.frame() %>% .[.$Real == 1 & .$Observed == 1, ]
+raster_occurences
+
+
+# The environmental variables are associated with the occurrences using their coordinates
+stack_occ <- brick(r1, r3, r4, r5)
+values_occ <- stack_occ %>% rasterToPoints() %>% as.data.frame()
+values_occ
+
+filtered_occ <- merge(values_occ, raster_occurences, by = c("x", "y"))
+
+occurrences_values <- filtered_occ[,-c(1:2, 7:8)]
+# occurrences_values <- occurrences_values[,-(1:2)]
+
+## Climatic values of the occurrences (step 3)
+nrow(occurrences_values)
+occurrences_values %>%  sample_n(size=1)
+
+# List of the occurrences we want to test
+valori_n_occ <- c(200, 500, 700, 990)
+
+lista_output_occ <- list()
+
+# Loop for the function 
+for (i in seq_along(valori_n_occ)) {
+  pluto <- pippo(occurrences_values, valori_n_occ[i])
+  lista_output_occ[[i]] <- pluto
+}
+
+lista_output_occ
+length(lista_output_occ)
+
+### Tutti insieme
+# df_total_occ <- do.call(rbind, lista_output_occ)
+
+# Plot
+ggplot(lista_output_occ[[3]], aes(x = n_occ, y = iperv)) +
+  geom_point() + # Punti
+  geom_smooth(method = "loess", se = TRUE) +  # Linea di interpolazione
+  labs(x = "n_occ", y = "iperv", title = "graph") +
+  theme_minimal()
+
+##################### Roadside ##################################
 # The occurrences (data.frame) are transformed into a SpatVector object
 coord_occ <- terra::vect(filtered_occ, geom = c("x","y"), crs="epsg:4326")
 coord_occ
+
 # For calculating distances in meters, it is necessary to change the CRS
 # from 4326 to 3857. Keeping WGS84 will result in distances in degrees
 coord_occ_dist <- terra::project(coord_occ, "EPSG:3857")
@@ -246,12 +265,13 @@ coord_occ_dist
 getwd()
 
 roads <- "shapefiles/strade_parma.shp" %>% st_read() %>% .$geometry %>% terra::vect()
-
+# roads <- (roads - cellStats(roads, 'mean')) / cellStats(roads, 'sd')
 # Here, the CRS also needs to be changed
 roads <- terra::project(roads, "EPSG:3857")
 
 # Visualization
 plot(roads)
+same.crs(roads, coord_occ_dist)
 points(coord_occ_dist, col = "red", pch = 16)
 
 # With this function, the distance of each point from all roads is obtained
@@ -266,11 +286,11 @@ summary(min_dist)
 coord_occ$distance <- min_dist
 
 # Based on statistics, the intervals' limits are established
-lim <- c(50000, 120000, 140000, 150000, 170000)
+lim <- c(0, 100, 1000, 2000, 5000, 10000, 20000, 27000)
 
 ## Sampling probability: since the probability of sampling decreases with distance from the road, we can imagine a decreasing logarithmic curve.
 # Sampling probability formula
-c <- 0.0001
+c <- 1
 sampling_prob <- 1-(((log(c*min_dist))/(log(max(c*min_dist)))))
 
 # Each point is assigned to its respective class
@@ -304,88 +324,112 @@ by_prob <- whole_data %>%
 # In each group, multiplying the initial number of points by the average sampling probability gives a smaller number of points per class
 count(by_prob, class)
 
-# In red are the original points, in blue are those actually sampled
+# In blue are the original points, in red are those actually sampled
 plot(roads)
 points(whole_data %>% terra::vect(., geom = c("x","y"), crs="epsg:4326") %>%
          terra::project(., "EPSG:3857"), col="blue")
 points(by_prob %>% terra::vect(., geom = c("x","y"), crs="epsg:4326") %>%
          terra::project(., "EPSG:3857"), col="red")
+dev.off()
+occurrences_values
+biased_df <- by_prob %>% terra::vect(., geom = c("x","y"), crs="epsg:4326") %>%
+  terra::project(., "EPSG:3857") %>% as.data.frame()
 
-points_closer_to_roads <- by_prob %>% terra::vect(., geom = c("x","y"), crs="epsg:4326") %>%
-  terra::project(., "EPSG:3857")
+biased_df <- biased_df[,-c(1:2, 7:12)]
+biased_df
 
-points_closer_to_roads
+nrow(biased_df)
 
-# Converti l'SpatVector in un dataframe
-df_points_closer <- as.data.frame(points_closer_to_roads)
+# List of the occurrences we want to test
+valori_n_occ_biased <- c(50, 100, 150, 250)
 
-# Stampare il dataframe
-print(df_points_closer)
-df_points_closer <- df_points_closer[,-1]
-df_points_closer <- df_points_closer[,-(7:11)]
+lista_output_biased <- list()
 
-df_points_closer
+# Loop for the function 
+for (i in seq_along(valori_n_occ_biased)) {
+  pluto <- pippo(biased_df, valori_n_occ_biased[i])
+  lista_output_biased[[i]] <- pluto
+}
+
+lista_output_occ
+length(lista_output_biased)
+
+lista_output_biased
+
+# Plot
+ggplot(lista_output_biased[[1]], aes(x = n_occ, y = iperv)) +
+  geom_point() + # Punti
+  geom_smooth(method = "loess", se = TRUE) +  # Linea di interpolazione
+  labs(x = "n_occ", y = "iperv", title = "graph") +
+  theme_minimal()
+
+# Unico dataframe
+dataframe_outputs_occ <- do.call(rbind, lista_output_occ)
+dataframe_outputs_biased <- do.call(rbind, lista_output_biased)
+
+# Combinati
+plot_combined <- ggplot() +
+  geom_point(data = dataframe_outputs_occ, aes(x = n_occ, y = iperv, color = "unbiased")) +
+  geom_point(data = dataframe_outputs_biased, aes(x = n_occ, y = iperv, color = "biased")) +
+  geom_smooth(data = dataframe_outputs_occ, aes(x = n_occ, y = iperv, color = "blue"), method = "loess", se = TRUE) +
+  geom_smooth(data = dataframe_outputs_biased, aes(x = n_occ, y = iperv, color = "red"), method = "loess", se = TRUE) +
+  labs(x = "n_occ", y = "iperv", color = "Dataset") +  
+  ggtitle("ipervolume") +  
+  theme_minimal() + 
+  theme_bw() +
+  scale_color_manual(values = c("unbiased" = "blue", "biased" = "red"),
+                     labels = c("unbiased" = "Unbiased", "biased" = "Biased"))
+
+print(plot_combined)
+
+dev.off()
+################### salva separatamente ######################
+# Inizializza una lista vuota per salvare i risultati
+# lista_output <- list()
+
+# Definisci i valori di n_occ desiderati
+# valori_n_occ <- c(80, 100, 200, 300, 400)
+
+# Esegui un ciclo for per calcolare pippo per ciascun valore di n_occ
+#for (i in seq_along(valori_n_occ)) {
+#  pluto <- pippo(occurrences_values, valori_n_occ[i])
+#  nome_variabile <- paste0("pluto", i)
+#  lista_output[[nome_variabile]] <- pluto
+# }
+
+
+####################################################################
 # Inizializza un vettore per salvare gli ipervolumi
-ipervolumi_roads <- numeric()
-num_occurrences_roads <- numeric()
+# ipervolumi <- numeric()
+# num_occurrences <- numeric()
 
 # Funzione per calcolare l'ipervolume
-calcola_ipervolume <- function(data) {
-  hv_occ <- hypervolume(data)
-  return(hv_occ@Volume)
-}
+# calcola_ipervolume <- function(data) {
+#  hv_occ <- hypervolume_gaussian(data)
+#  return(hv_occ@Volume)
+# }
+# end <- as.numeric(nrow(occurrences_values))
+# end
 
-
-end_roads <- as.numeric(nrow(df_points_closer))
-
-filtered_occ
-
-df_points_closer
-
-ipervolumi_roads <- c()
-num_occurrences_roads <- c()
-# Ciclo for per incrementare il numero di occorrenze da 10 a 100
-# Partire da 10
-for (j in seq(10, end_roads, 15)) {
+# Ciclo for per incrementare il numero di occorrenze
+# for (i in seq(10, end, 100)) {
   # Seleziona le prime 'i' occorrenze
-  subset_occ_roads <- df_points_closer[1:j, ]
+#  subset_occ <- occurrences_values[1:i, ]
   
   # Calcola l'ipervolume per il subset di occorrenze
-  hv_roads <- calcola_ipervolume(subset_occ_roads)
+ #  hv <- calcola_ipervolume(subset_occ)
   
   # Salva l'ipervolume e il numero di occorrenze
-  ipervolumi_roads <- c(ipervolumi_roads, hv_roads)
-  num_occurrences_roads <- c(num_occurrences_roads, j)
-}
+  # ipervolumi <- c(ipervolumi, hv)
+  # num_occurrences <- c(num_occurrences, i)
+# }
 
-# Crea il dataframe
-df_closer <- data.frame(n_occurrences_roads = num_occurrences_roads, hypervolume_roads = ipervolumi_roads)
-
+# Dataframe
+# df <- data.frame(n_occurrences = num_occurrences, hypervolume = ipervolumi)
 # Stampare il dataframe
-print(df_closer)
+# print(df)
 
-# Creazione del plot con ggplot
-plot_1 <- ggplot(df_closer, aes(x = n_occurrences_roads, y = hypervolume_roads)) +
-  geom_line() +  # Linea spezzata
-  geom_point() + # Punti
-  labs(x = "Numero di Occorrenze", y = "Ipervolume") +  # Etichette degli assi
-  ggtitle("Ipervolume all'aumentare delle occorrenze") +   # Titolo del grafico
-  theme_minimal() +  # Stile del tema, minimalista 
-  theme_bw()
 
-# Visualizza il plot
-print(plot_1)
-
-# Creazione del plot con ggplot sovrapposto
-plot_combined <- ggplot() +
-  geom_line(data = df, aes(x = n_occurrences, y = hypervolume), color = "blue", linetype = "solid") +
-  geom_point(data = df, aes(x = n_occurrences, y = hypervolume), color = "blue") +
-  geom_line(data = df_closer, aes(x = n_occurrences_roads, y = hypervolume_roads), color = "red", linetype = "dashed") +
-  geom_point(data = df_closer, aes(x = n_occurrences_roads, y = hypervolume_roads), color = "red") +
-  labs(x = "Numero di Occorrenze", y = "Ipervolume") +  
-  ggtitle("Ipervolume all'aumentare delle occorrenze") +  
-  theme_minimal() + 
-  theme_bw()
-
-# Visualizzazione del plot combinato
-print(plot_combined)
+# Output List 
+## Bootstrap con incremento
+## Regressione polinomiale 
