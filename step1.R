f80dc91327b9ecd71246a2762621266ab1e6291d
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2024-11-30 17:25:19 +0100

    Update step1.R

--- step1.R
+++ step1.R
@@ -1,6 +1,6 @@
 # first step: random species generation
 # each species has a number of occurrences
-# the output is a csv of points in which we can find: coordinates of the points, distance from the roads, probability to be sampled,
+# the output is a csv of points in which we can find; coordinates of the points, distance from the roads, probability to be sampled,
 # bioclimatic variables, type of point according to the lazy sampler simulation (unbiased/biased)
 # parameters to be setted before: number of starting occurrences, species prevalence, sample prevalence
 # those infos will be inside the name of the csv
@@ -32,6 +32,7 @@ abruzzo_bb <- st_bbox(aoi_abruzzo)
 # from OSM select type of roads: primary, secondary, tertiary (paths)
 ht_secondary <- "secondary"
 
+
 # download roads from OSM 
 osm_abruzzo <- oe_get("Abruzzo", stringsAsFactors = FALSE, quiet = TRUE)
 osm_abruzzo_roads <- osm_abruzzo[osm_abruzzo$highway %in% ht_secondary, ]
@@ -54,12 +55,9 @@ mydata <- raster::stack(r_list)
 aoi_sp <- sf::as_Spatial(aoi_abruzzo)
 mydata <- mydata %>% crop(., aoi_sp) %>% mask(., aoi_sp)
 
-# original data: will be useful later (AOA)
-mydata_backup <- mydata
-
-
 # distances
 roads_vect <- terra::vect(osm_abruzzo_roads$geometry)
+
 raster_roads <- as(mydata_backup[[1]], "SpatRaster")
 
 r <- terra::rasterize(roads_vect, raster_roads)
@@ -81,16 +79,16 @@ prob_raster <- classify(d, cbind(values(d), sampling_prob))
 
 # parameters to be saved
 # species prevalence
-sp_prevalence <- 0.1
+sp_prevalence <- 0.30
 
 # sample prevalence
 sample_prev <- 0.9
 
 # n. occurrences
-n_occ <- 300
+n_occ <- 100
 
 # number of species
-n_species <- 4
+n_species <- 2
 
 # cicle for virtual species generation 
 for (species_num in 1:n_species) {
@@ -120,11 +118,6 @@ for (species_num in 1:n_species) {
                                        correct.by.suitability = TRUE,
                                        plot = FALSE)
   
-  # Z-transform bioclimatic variables
-  for (i in 1:nlayers(mydata)) {
-    mydata[[i]] <- (mydata[[i]] - cellStats(mydata[[i]], 'mean')) / cellStats(mydata[[i]], 'sd')
-  }
-  
   # occurrences as points
   initial_occ <- presence.points$sample.points %>%
     as.data.frame() %>%
@@ -146,6 +139,15 @@ for (species_num in 1:n_species) {
   # rename
   names(initial_occ_bio_var)[names(initial_occ_bio_var) == "."] <- "distance"
   
+  # add suitability
+  initial_occ_bio_var <- random.sp$suitab.raster %>%
+    terra::extract(., initial_occ_bio_var) %>%
+    cbind(initial_occ_bio_var, .)
+  
+  # rename
+  names(initial_occ_bio_var)[names(initial_occ_bio_var) == "VSP.suitability"] <- "suitability"
+
+  
   # add probability to be sampled for each point
   initial_occ_bio_var <- prob_raster %>%
     terra::extract(., initial_occ_bio_var) %>%
@@ -166,10 +168,13 @@ for (species_num in 1:n_species) {
   
   # dataframe
   df <- as.data.frame(st_coordinates(initial_occ_bio_var))
+
+    # add variables and drop the 'geometry' column 
+  df <- cbind(df, initial_occ_bio_var
+               [, !(names(initial_occ_bio_var) %in% c("geometry"))]
+              ) 
   
-  # add variables
-  df <- cbind(df, initial_occ_bio_var[, !(names(initial_occ_bio_var) %in% c("geometry"))])
-  
+  df$geometry <- NULL
   # save as csv
   # the name will include: the number of the species, species prevalence, sample prevalence and number of initial occurrences
   file_name <- paste0("species_", species_num, "_sp_prevalence_", sp_prevalence, 
@@ -177,4 +182,3 @@ for (species_num in 1:n_species) {
   write.csv(df, file_name, row.names = FALSE)
 
 }
-
