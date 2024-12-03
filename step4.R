bbb947f9a9267f4dc9b01c475831f834fce51cf6
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2024-12-03 12:37:04 +0100

    Update step4.R

--- step4.R
+++ step4.R
@@ -20,6 +20,8 @@ plot(sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa)
 # select aoa from the stack
 sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa
 
+
+### AOA
 aoa_null   <- sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa$AOA_null
 aoa_biased <- sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa$AOA_biased
 aoa_all    <- sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa$AOA_all
@@ -78,10 +80,9 @@ pixel_stat <- function(raster_stack, id) {
 results <- pixel_stat(sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa, id = "sp2_0.3_0.9_350")
 print(results)
 
-#            ID           Common_Pixels      Exclusive_Null  Exclusive_Biased  Count_All
-#          sp2_0.3_0.9_350      5162            201              119            5790
 
-# DI
+
+### DI
 di_null   <- sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa$DI_null
 di_biased <- sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa$DI_biased
 
@@ -110,6 +111,68 @@ ggplot(difference_DI_df, aes(x = x, y = y, fill = DI_null)) +
   coord_equal() +
   labs(title = "Difference in DI between AOA_null and AOA_biased")
 
+
+# DI stats
+analyze_DI <- function(raster_stack, species_id) {
+ 
+  # DI null DI biased
+  di_null <- raster_stack$DI_null
+  di_biased <- raster_stack$DI_biased
+  
+  # difference
+  difference_DI <- di_null - di_biased
+  
+  # na.remove
+  total_pixels <- sum(!is.na(values(difference_DI)))
+  
+  # DI null > DI biased
+  pixels_null_greater <- sum(values(di_null) > values(di_biased), na.rm = TRUE)
+  
+  # DI_null < DI_biased
+  pixels_biased_greater <- sum(values(di_null) < values(di_biased), na.rm = TRUE)
+  
+  # Pixel invariati (DI_null == DI_biased)
+  unchanged_pixels <- sum(values(di_null) == values(di_biased), na.rm = TRUE)
+  
+  # %
+  percent_null_greater <- (pixels_null_greater / total_pixels) * 100
+  percent_biased_greater <- (pixels_biased_greater / total_pixels) * 100
+  percent_unchanged <- (unchanged_pixels / total_pixels) * 100
+  
+  # mean and sd
+  mean_null <- mean(values(di_null), na.rm = TRUE)
+  mean_biased <- mean(values(di_biased), na.rm = TRUE)
+  sd_null <- sd(values(di_null), na.rm = TRUE)
+  sd_biased <- sd(values(di_biased), na.rm = TRUE)
+  
+  # data frame
+  result <- data.frame(
+    Species_ID = species_id,
+    Mean_DI_null = mean_null,
+    SD_DI_null = sd_null,
+    Mean_DI_biased = mean_biased,
+    SD_DI_biased = sd_biased,
+    Pixels_null_greater = pixels_null_greater,
+    Percent_null_greater = percent_null_greater,
+    Pixels_biased_greater = pixels_biased_greater,
+    Percent_biased_greater = percent_biased_greater,
+    Unchanged_pixels = unchanged_pixels,
+    Percent_unchanged = percent_unchanged
+  )
+  
+  return(result)
+}
+
+# example
+species_id <- "sp2_0.3_0.9_350"  # ID della specie
+result <- analyze_DI(sp2_sp_prev0.3_sample_prev0.9_nocc350_aoa, species_id)
+
+# csv
+write.csv(result, "DI_analysis_species.csv", row.names = FALSE)
+
+
+
+# georg lm
 coords <- difference_DI_df %>% dplyr::select(Longitude = x, Latitude = y)
 
 data.wgs <- SpatialPointsDataFrame(
